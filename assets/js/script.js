193203319d0505fbe7ce3890acd8cd203f8515a2
Brian Halpin <brian.t.halpin@gmail.com>
2022-02-28 15:36:39 -0500

    Add fetch requests for OMDB and Youtube API. Return and log data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,50 @@
-console.log("ok")
+// 
+// HTML Element selectors
+// 
+
+// Constants
+const movieTitle = 'shrek'
+const youtubeAPI = 'AIzaSyARoCQOMM8wFTSsLyefC3mTZPCsXhr_pYg'
+
+
+// YouTube Search API
+function searchVideos(year,videoType){
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${movieTitle} ${year} ${videoType}&key=${youtubeAPI}&type=video`)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data){
+        console.log('youtube',data)
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+// OMDB api
+function searchMovie(){
+    fetch(`http://www.omdbapi.com/?apikey=6c411e7c&s=${movieTitle}`)
+        .then(function(response) {
+            return response.json()
+        }).then(function(data) {
+            console.log('OMDB',data)
+
+            // Extracting the year from first omdb result.  Change later to Whichever result the user clicks on
+            const year = data.Search[0].Year
+
+            // Pulls up a video for each type
+            searchVideos(year,'Trailer')
+            searchVideos(year,'Clips')
+            searchVideos(year,'Review')
+
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+searchMovie()
+
+
