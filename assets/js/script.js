d39d1d7917563f0a836004e1d505ae9511827ba5
Sami Fares <djsamif@gmail.com>
2022-03-01 14:20:33 -0500

    grabbed and replaced HTML and showing on cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
+const posterSearchEl = document.querySelectorAll(".poster-search");
+posterSearchEl[0].setAttribute("src", "https://bulma.io/images/placeholders/480x480.png");
+
+const titleSearchEl = document.querySelectorAll(".movie");
+
+const searchYearEl = document.querySelectorAll(".releaseYear");
+
+
+
+
 // 
 // HTML Element selectors
 // 
 
 // Constants
-const movieTitle = 'shrek' //Change to user input value
+const movieTitle = 'Role Models' //Change to user input value
 const youtubeAPI = 'AIzaSyARoCQOMM8wFTSsLyefC3mTZPCsXhr_pYg'
 
 // Results variables
@@ -47,6 +57,7 @@ function getMovieDetails(movieId){
 
 // OMDB api
 function searchMovie(){
+    console.log("searchMovie");
     fetch(`http://www.omdbapi.com/?apikey=6c411e7c&s=${movieTitle}`)
         .then(function(response) {
             return response.json()
@@ -54,8 +65,13 @@ function searchMovie(){
             console.log('OMDB',data)
 
             // Checks if a movie was found
-            if (data.Response === 'True'){
-                
+            if (true){
+                for(i=0; i<3; i++) {
+                    titleSearchEl[i].textContent= data.Search[i].Title
+                    posterSearchEl[i].setAttribute("src", data.Search[i].Poster)
+                    searchYearEl[i].textContent= data.Search[i].Year
+                   
+                }
                 // Random Id
                 const movieId = data.Search[0].imdbID
                 // Extracting the year from first omdb result.  Change later to Whichever result the user clicks on
